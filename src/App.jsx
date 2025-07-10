5e2ec471601c9936672f7d600664f18f48dae8d2
a01040579861 <a01040579861@gmail.com>
2025-07-10 20:51:13 +0900

    Personal color semi project start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <div>
+      <h1>퍼스널컬러 세미 프로젝트</h1>
+    </div>
+  );
+};
+
+export default App;
